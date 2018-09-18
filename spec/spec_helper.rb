deebe658314030c546c01135f9be40bc06a2a418
marii¨ <mnyrop@users.noreply.github.com>
2018-09-18 16:17:28 -0400

    add ffox opt

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,14 +7,25 @@ require 'rack/test'
 RSpec.configure do |config|
   config.include Capybara::DSL
 
-  Capybara.register_driver :headless_chrome do |app|
-    capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-      chromeOptions: { args: %w(headless disable-gpu) }
-    )
-    Capybara::Selenium::Driver.new app, browser: :chrome, desired_capabilities: capabilities
+  if ENV['driver'] == 'chrome'
+    Capybara.register_driver :headless_chrome do |app|
+      capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+        chromeOptions: { args: %w(headless disable-gpu) }
+      )
+      Capybara::Selenium::Driver.new app, browser: :chrome, desired_capabilities: capabilities
+    end
+
+    Capybara.javascript_driver = :headless_chrome
+  else
+    Capybara.register_driver :firefox_headless do |app|
+      options = ::Selenium::WebDriver::Firefox::Options.new
+      options.args << '--headless'
+      Capybara::Selenium::Driver.new(app, browser: :firefox, options: options)
+    end
+
+    Capybara.javascript_driver = :firefox_headless
   end
 
-  Capybara.javascript_driver = :headless_chrome
   Capybara.current_driver = Capybara.javascript_driver
   Capybara.app = Rack::Jekyll.new(:force_build => false)
   Capybara.server = :webrick
